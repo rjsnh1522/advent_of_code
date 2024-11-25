1c89565def8e78db3c78750c931911c0e7a3372c
pawan <rjsnh1522@gmail.com>
2024-11-25 23:38:28 +0530

    2023: Day 1

--- 2023/Day 1: Trebuchet?!/Trebuchet.py
+++ 2023/Day 1: Trebuchet?!/Trebuchet.py
@@ -1,29 +1,17 @@
 
 def find_first_and_last_digit(line):
-    left, right = 0, len(line)-1
-    f_d = None
-    r_d = None
-    while left < right:
-        if f_d is not None and r_d is not None:
-            break
-        else:
-            if f_d is None and line[left].isdigit():
-                f_d = int(line[left])
-            if r_d is None and line[right].isdigit():
-                r_d = int(line[right])
-            right -= 1
-            left += 1
-    print(f"Number at each line {f_d}{r_d}")
-    if r_d is None:
-        r_d = f_d
-    return int(f"{f_d}{r_d}")
+    digits = []
+    for i in line:
+        if i.isdigit():
+            digits.append(i)
+    return int(f"{digits[0]}{digits[-1]}")
 
 
 def find_sum():
     total = 0
     with open('input.txt', 'r') as file:
         for i in file.readlines():
-            number = find_first_and_last_digit(i)
+            number = find_first_and_last_digit(i.strip())
             total += number
 
     return total

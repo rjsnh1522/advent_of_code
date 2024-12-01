d0b3c760bdf44af46d56490f031343c78b7d734d
pawan <rjsnh1522@gmail.com>
2024-12-01 12:31:37 +0530

    2024: Day 1

---
+++ 2024/Historian Hysteria_p1.py
@@ -0,0 +1,24 @@
+def read_input():
+    left_heap = []
+    right_heap = []
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            if line:
+                left, right = line.strip().split()
+                left_heap.append(int(left))
+                right_heap.append(int(right))
+    return left_heap, right_heap
+
+
+def caller():
+    left, right = read_input()
+    left.sort()
+    right.sort()
+    total_diff = 0
+    for i in range(len(left)):
+        total_diff += abs(left[i] - right[i])
+    return total_diff
+
+
+if __name__ == "__main__":
+    total = caller()
---
+++ 2024/Historian Hysteria_p2.py
@@ -0,0 +1,27 @@
+def read_input():
+    left_heap = []
+    # heapify(left_heap)
+    right_heap = []
+    # heapify(right_heap)
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            if line:
+                left, right = line.strip().split()
+                left_heap.append(int(left))
+                right_heap.append(int(right))
+    return left_heap, right_heap
+
+
+def caller():
+    left, right = read_input()
+    left.sort()
+    right.sort()
+    total_sum = 0
+    for i in range(len(left)):
+        count = right.count(left[i])
+        total_sum += (left[i] * count)
+    return total_sum
+
+
+if __name__ == "__main__":
+    total = caller()
